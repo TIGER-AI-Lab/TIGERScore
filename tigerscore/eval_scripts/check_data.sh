895f8476611b20f30a8fcd436eb64b215cdaa59d
Dongfu <dongfu.jdf@gmail.com>
2023-11-14 01:23:07 -0500

    add check data script

---
+++ tigerscore/eval_scripts/check_data.sh
@@ -0,0 +1,11 @@
+python check_data.py \
+    --input_file "../../data/train_synthetic.jsonl" \
+    --output_file "../../data/train_synthetic.check.jsonl" \
+    --model_name "gpt-4" \
+    --num_samples 1000
+
+
+# python check_data.py \
+#     --input_file "../../data/train_mix.jsonl" \
+#     --output_file "../../data/train_mix.check.jsonl" \
+#     --model_name "ChatGPT"
