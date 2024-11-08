5d27af1d721e6df1cbbfec69899a012d8163bf23
Dongfu <dongfu.jdf@gmail.com>
2024-11-08 12:16:36 -0500

    update

--- tigerscore/finetune/test_llama_vllm.sh
+++ tigerscore/finetune/test_llama_vllm.sh
@@ -5,13 +5,19 @@
 #SBATCH --output=../../jobs/test_llama/%j.out
 nvidia-smi
 
+
+## Note
+# please download the data in the working directory as indicated in the Data Preparation section in the read me
+# quick command: gdown https://drive.google.com/uc?id=1DAjvig-A_57CuBvENLg8A2PycOaz9ZkT
+## 
+
 model_name="meta-llama/Llama-2-7b-hf"
 outputs_dir=""
 
 # outputs_dir="../../outputs"
 checkpoint_name="ref"
 # checkpoint_path="${outputs_dir}/${model_name}/${checkpoint_name}/checkpoint-532"
-checkpoint_path="TIGER-Lab/TIGERScore-13B-V1.0"
+checkpoint_path="TIGER-Lab/TIGERScore-13B"
 
 human_score_names="gpt_rank_score"
 data_path="../../data/evaluation/lfqa/test_data_prepared.json"
@@ -84,6 +90,21 @@ python test_llama_vllm.py \
 task="data2text"
 human_score_names="Correctness,DataCoverage,Fluency,Relevance,TextStructure"
 data_path="../../data/evaluation/d2t/webnlg_2020/test_data_prepared.json"
+output_path="${data_path}.llama_2_7b_${checkpoint_name}_test.output"
+python test_llama_vllm.py \
+    --model_name_or_path ${checkpoint_path} \
+    --task ${task} \
+    --data_path ${data_path} \
+    --output_path ${output_path} \
+    --batch_size 60 \
+    --human_score_names ${human_score_names} \
+    --overwrite
+
+
+# storygen test human
+task="storygen"
+human_score_names="human"
+data_path="../../data/evaluation/storygen/test_data_prepared.json"
 output_path="${data_path}.llama_2_7b_${checkpoint_name}_test.output"
 python test_llama_vllm.py \
     --model_name_or_path ${checkpoint_path} \
